a5ea819a4f718b0df186ce03d49c2658a07a7894
Matt Rothenberg <mattrothenberg@gmail.com>
2016-10-02 17:52:17 -0400

    Refactor header

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
           <div className="px2 sm-px3 py2">
             <div className="clearfix">
               <div className="col col-12 sm-col-7 sm-pr4">
-                <h3 className="h4 caps gray regular">About Carol</h3>
+                <h3 className="h4 caps gray medium">About Carol</h3>
                 <hr className="hr-stubby mt1 mb3 faded"/>
                 <p className="h3">Raised in New York City, I received my degree in classical theater from Boston University. I spent many years performing in NY, Boston and in tours around the country. I have voiced dozens of Radio/TV commercials and worked in Radio for CBS News.
                 For the past 20 years I have  been recording audiobooks for the Library of Congress for The National Library Service reading books of every genre including Romance,Suspense, Self-Help, Children's books, and teen distopian fantasy. You name the genre, I have recorded it.</p>
@@ -21,7 +21,7 @@ class App extends Component {
                 <p className="h3">I am a proud member of SAG/AFTRA and a member of the Audiobook Producers Association of America (APA)</p>
               </div>
               <div className="col col-12 sm-col-5 sm-pl2">
-                <h3 className="h4 caps gray regular">Sample Work</h3>
+                <h3 className="h4 caps gray medium">Sample Work</h3>
                 <hr className="hr-stubby mt1 mb3 faded"/>
                 <WorkSample src={paperLoveClip} category={"Romance"}/>
                 <WorkSample src={paperLoveClip} category={"Harlequin"}/>
--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ class Header extends Component {
 		return(
 			<div className="hero px2 pb2 sm-py0 sm-px0">
 			  <div className="flex col-10 sm-col-7 flex-column sm-px3 hero-left justify-end sm-pb4">
-			    <div className="max-width-4">
+			    <div className="col-12 sm-col-8 md-col-7 lg-col-6">
 			      <h1 className="mt0 mb0 lift">Carol Dines</h1>
 			      <hr className="hr-stubby mt1 mb2"/>
 			      <h2 className="my0 lift regular">Actress/Audiobook Narrator living in Louisville, KY</h2>
